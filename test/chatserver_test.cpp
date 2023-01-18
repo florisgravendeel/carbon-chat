2b9e9ac215cffa2ff0b971ee9da4f666d891ed25
florisgravendeel <florisgra@gmail.com>
2023-01-18 12:38:57 +0100

    Setup tests with dummy test

---
+++ test/chatserver_test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Floris Gravendeel on 18/01/2023.
+//
+#define BOOST_TEST_MODULE TestChatServer
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(ChatServerCase)
+{
+    // To simplify this example test, let's suppose we'll test 'float'.
+    // Some test are stupid, but all should pass.
+    float x = 9.5f;
+
+    BOOST_CHECK(x != 0.0f);
+    BOOST_CHECK_EQUAL((int)x, 9);
+    BOOST_CHECK_CLOSE(x, 9.5f, 0.0001f); // Checks differ no more then 0.0001%
+}
