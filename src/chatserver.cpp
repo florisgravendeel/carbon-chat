505c3347e48f51021cad3135c5324091fdc72094
florisgravendeel <florisgra@gmail.com>
2023-01-19 16:50:32 +0100

    Add logger

--- src/chatserver.cpp
+++ src/chatserver.cpp
@@ -13,52 +13,62 @@ ChatServer::ChatServer(int port, boost::mutex &mutex) {
     server.init_asio();
 
     // Register Event Handlers
-    server.set_open_handler(bind(&ChatServer::on_successful_new_connection, this, std::placeholders::_1));
-    server.set_close_handler(bind(&ChatServer::on_close_connection, this, std::placeholders::_1));
-    server.set_message_handler(bind(&ChatServer::on_message, this, std::placeholders::_1, std::placeholders::_2));
+    server.set_open_handler(bind(&ChatServer::on_successful_new_connection, this, placeholders::_1));
+    server.set_close_handler(bind(&ChatServer::on_close_connection, this, placeholders::_1));
+    server.set_fail_handler(bind(&ChatServer::on_connection_failed, this, placeholders::_1));
+    server.set_message_handler(bind(&ChatServer::on_message, this, placeholders::_1, placeholders::_2));
 }
 
 void ChatServer::start() {
-    std::cout<<"Starting server on ws://localhost:"<< port;
-    std::cout<<std::endl;
+    log("Starting server on ws://localhost:" + to_string(port));
     server.listen(port);
     server.start_accept();
+
     server.run();
 
 }
 void ChatServer::stop() {
-    std::cout <<  "Stopping server!" << std::endl;
+    log("Stopping server!");
     server.stop_listening();
-//    server.close() Close all connections
-
+    for (const auto& conn : connection_list){
+        server.close(conn, websocketpp::close::status::going_away, "Server is shutting down.");
+    }
+}
+void ChatServer::log(const std::string& message) {
+    server.get_alog().write(websocketpp::log::alevel::app, message);
 }
 
 int ChatServer::get_total_connections() const {
     return 0;
 }
 
-void ChatServer::send_message(const std::string &msg) {
+void ChatServer::send_message(const string &msg) {
 
 }
 
-void ChatServer::broadcast_message(const std::string &msg) {
-
+void ChatServer::broadcast_message(const string &msg) {
+    log("[broadcast_message] " + msg);
+    for (const auto& conn : connection_list){
+        server.send(conn, msg, websocketpp::frame::opcode::text);
+    }
 }
 
-void ChatServer::on_successful_new_connection(Connection connection) {
-
+void ChatServer::on_successful_new_connection(const Connection& connection) {
+    connection_list.insert(connection);
+    string msg = "A user joined the chatroom. Total online users: " + to_string(connection_list.size());
+    broadcast_message(msg);
 }
 
-void ChatServer::on_connection_failed(Connection connection) {
-
+void ChatServer::on_connection_failed(const Connection& connection) {
+    string msg = "Connection failed for client"; //TODO: log ip
+    log(msg);
 }
 
-void ChatServer::on_close_connection(Connection connection) {
-
+void ChatServer::on_close_connection(const Connection& connection) {
+    connection_list.erase(connection);
 }
 
-void ChatServer::on_message(const Connection& connection, Message message) {
-    std::cout << "Message received on connection(" << connection.lock().get() << "): " + message->get_payload() << std::endl;
-
+void ChatServer::on_message(const Connection& connection, const Message& message) {
+    log(message->get_payload());
 }
 
--- src/chatserver.h
+++ src/chatserver.h
@@ -10,10 +10,12 @@
 #include <websocketpp/server.hpp>
 #include <websocketpp/endpoint.hpp>
 #include <boost/thread/mutex.hpp>
+#include "set"
 
 typedef websocketpp::server<websocketpp::config::asio> Server;
 typedef websocketpp::connection_hdl Connection;
 typedef Server::message_ptr Message;
+typedef std::set<Connection,std::owner_less<Connection>> ConnectionList;
 
 /**
  * All documentation has been copied from: https://docs.websocketpp.org/reference_8handlers.html
@@ -34,25 +36,31 @@ private:
 
     /// Either open or fail will be called for each connection. Never both. All connections that
     /// begin with an open handler call will also have a matching close handler call when the connection ends.
-    void on_successful_new_connection(Connection connection);
+    void on_successful_new_connection(const Connection& connection);
 
     /// Either open or fail will be called for each connection. Never both.
     /// Connections that fail will never have a close handler called.
-    void on_connection_failed(Connection connection);
+    void on_connection_failed(const Connection& connection);
 
     /// Close will be called exactly once for every connection that open was called for.
     /// Close is not called for failed connections.
-    void on_close_connection(Connection connection);
+    void on_close_connection(const Connection& connection);
 
     /// These handers are called in response to incoming messages or message like events.
     /// They only will be called while the connection is in the open state.
     /// Applies to all non-control messages, including both text and binary opcodes.
     /// The message_ptr type and its API depends on your endpoint type and its config.
-    void on_message(const Connection& connection, Message message);
+    void on_message(const Connection& connection, const Message& message);
+
+    /// WebSocket++ has the capability of logging events during the lifetime of the connections that it processes.
+    /// Each endpoint has two independent logging interfaces that are used by all connections created by that endpoint.
+    /// The method uses the an access interface for application specific logs.
+    void log(const std::string& message);
 
     Server server;
     int port;
     boost::mutex* mutex;
+    ConnectionList connection_list;
 };
 
 #endif //CARBONCHAT_CHATSERVER_H
