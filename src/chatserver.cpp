08d6436a953cec3cb7217e749b8297e08e5268ed
florisgravendeel <florisgra@gmail.com>
2023-01-19 14:20:07 +0100

    Add thread-safe stop command (with mutex)

--- src/chatserver.cpp
+++ src/chatserver.cpp
@@ -1,11 +1,64 @@
 #include "chatserver.h"
 #include <iostream>
+using namespace std;
 
-ChatServer::ChatServer(int port) {
-    ChatServer::port = port;
-    run();
-};
+ChatServer::ChatServer(int port, boost::mutex &mutex) {
+    this->port = port;
+    this->mutex = &mutex;
+    // Set logging settings
+    server.set_access_channels(websocketpp::log::alevel::all);
+    server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
-void ChatServer::run() {
-    std::cout << &"Starting server on port " [ port];
+    // Initialize Asio
+    server.init_asio();
+
+    // Register Event Handlers
+    server.set_open_handler(bind(&ChatServer::on_successful_new_connection, this, std::placeholders::_1));
+    server.set_close_handler(bind(&ChatServer::on_close_connection, this, std::placeholders::_1));
+    server.set_message_handler(bind(&ChatServer::on_message, this, std::placeholders::_1, std::placeholders::_2));
 }
+
+void ChatServer::start() {
+    std::cout<<"Starting server on ws://localhost:"<< port;
+    std::cout<<std::endl;
+    server.listen(port);
+    server.start_accept();
+    server.run();
+
+}
+void ChatServer::stop() {
+    std::cout <<  "Stopping server!" << std::endl;
+    server.stop_listening();
+//    server.close() Close all connections
+
+}
+
+int ChatServer::get_total_connections() const {
+    return 0;
+}
+
+void ChatServer::send_message(const std::string &msg) {
+
+}
+
+void ChatServer::broadcast_message(const std::string &msg) {
+
+}
+
+void ChatServer::on_successful_new_connection(Connection connection) {
+
+}
+
+void ChatServer::on_connection_failed(Connection connection) {
+
+}
+
+void ChatServer::on_close_connection(Connection connection) {
+
+}
+
+void ChatServer::on_message(const Connection& connection, Message message) {
+    std::cout << "Message received on connection(" << connection.lock().get() << "): " + message->get_payload() << std::endl;
+
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,36 @@
 // Created by Floris Gravendeel on 18/01/2023.
 //
 #include "chatserver.cpp"
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
+using namespace std;
+
+ChatServer* server;
+boost::mutex s_mutex;
+
+void start_thread(){
+    // Create instance of Chatserver.
+    server = new ChatServer(9002, s_mutex);
+    server->start();
+}
 
 int main() {
-    // Create instance of Chatserver.
-    ChatServer server(9002);
+
+    boost::thread server_thread(start_thread);
+
+    string input;
+    cout << "To stop the server. Use command: stop\n";
+    while (true)
+    {
+        getline(cin, input);
+        if (input == "stop"){
+            s_mutex.lock();
+            server->stop();
+            s_mutex.unlock();
+            break;
+        }
+        cout << "Invalid command." << endl;
+    }
+
     return 0;
 }
