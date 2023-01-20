7ace2ccd50ed9e546d959c0d8375fc7a1ac75665
florisgravendeel <florisgra@gmail.com>
2023-01-20 11:13:38 +0100

    Improving the chat client (code messy)

--- src/client.py
+++ src/client.py
@@ -1,6 +1,9 @@
 import asyncio
+import signal
+
 import click as cmd
 import websockets
+from aioconsole import ainput
 from colorama import Fore
 
 PROMPT_COLOR: str = "yellow"
@@ -14,19 +17,39 @@ async def connect_to_chat(server_ip: str, username: str):
     cmd.secho(cmd.style('Connecting to ' + server_ip, fg=INFO_COLOR, reset=True) + "..")
 
     try:
-        async with websockets.connect("ws://" + server_ip + ":9002") as websocket:
+        async with websockets.connect("ws://" + server_ip + ":8080") as websocket:
             cmd.secho(cmd.style("Chatserver online", fg=SUCCESS_COLOR))
-            while True:
-                # Close socket on KeyBoardInterrupt
-                user_input: str = cmd.prompt(Fore.CYAN + "You" + Fore.LIGHTCYAN_EX + ": ", prompt_suffix="")
+            # cmd.secho(cmd.style("To disconnect and client stop the client, type: /stop", fg=INFO_COLOR))
+            # loop.add_signal_handler(signal.SIGTERM, loop.create_task, websocket.close())
+            # asyncio.create_task(open_chat_prompt(websocket, username))
+            await asyncio.gather(read_chat_messages(websocket), open_chat_prompt(websocket, username))
+            # await asyncio.gather(open_chat_prompt(websocket, username), read_chat_messages(websocket))
+            # while True:
+            #     cmd.echo(await websocket.recv())
+            # asyncio.create_task(read_chat_messages(websocket))
 
-                if len(user_input) == 0:
-                    cmd.secho(cmd.style("You cannot send an empty message!", fg=FAIL_COLOR))
-                else:
-                    await websocket.send(username + ": " + user_input)
-                await websocket.recv() # Read messages
     except (websockets.ConnectionClosed, OSError) as exception:
         cmd.secho(cmd.style("Chatserver offline. Stopping client", fg=FAIL_COLOR))
+
+
+async def open_chat_prompt(websocket: websockets, username: str):
+    while True:
+        user_input: str = cmd.prompt(Fore.CYAN + "You" + Fore.LIGHTCYAN_EX + ": ", prompt_suffix="")
+
+        if len(user_input) == 0:
+            cmd.secho(cmd.style("You cannot send an empty message!", fg=FAIL_COLOR))
+        # elif user_input == "/stop":
+        #     cmd
+        else:
+            await websocket.send(username + ": " + user_input)
+
+
+async def read_chat_messages(websocket: websockets):
+    while True:
+        cmd.echo(await websocket.recv())
+    # while True:
+    #     async for message in websocket:
+    #         cmd.secho(message)
 
 
 def configure_client():
@@ -53,8 +76,36 @@ def configure_client():
     # cmd.prompt(cmd.style("You"))
 
 
-if __name__ == '__main__':
-    try:
-        configure_client()
-    except cmd.exceptions.Abort:
-        cmd.secho(cmd.style("\nExiting client", fg=INFO_COLOR))
+# if __name__ == '__main__':
+#     try:
+#         configure_client()
+#     except cmd.exceptions.Abort:
+#         cmd.secho(cmd.style("\nExiting client", fg=INFO_COLOR))
+class PeriodicMessage:
+    def __init__(self, message="This is a periodic message. Enter new text to modify it."):
+        self.message = message
+
+    async def send_message(self, sleep_delay=3):
+        while True:
+            print(self.message)
+            await asyncio.sleep(sleep_delay)
+
+    async def read_message(self):
+        while True:
+            print("\nTest test 1 2 3")
+            await asyncio.sleep(3)
+
+    async def open_chat_prompt(self):
+        while True:
+            self.message = await ainput()
+
+    async def main(self):
+        await asyncio.gather(self.send_message(), self.open_chat_prompt(), self.read_message())
+
+
+
+if __name__ == "__main__":
+
+    periodic_msg = PeriodicMessage()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(periodic_msg.main())
